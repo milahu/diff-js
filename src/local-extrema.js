d3b6f742a4f1bcb9025e66e374aa88e55fe526e1
milahu <milahu@gmail.com>
2021-03-07 13:53:45 +0100

    localExtrema: use custom data getters, rename localExtremaXY to localExtrema

--- src/local-extrema.js
+++ src/local-extrema.js
@@ -10,71 +10,44 @@ import { getArray } from './shared.js';
 /**
  * Find local maxima and minima in a list of values.
  *
- * Javascript implementation of:
- * "A Linear-Time Algorithm That Locates Local Extrema
- * of a Function of One Variable From Interval Measurement
- * Results" - Karen Villaverde, Vladik Kreinovich
- * (Interval Computations 01/1993; 1993(4))
- *
- * Takes an object or array with numbers and returns an object
- * with two lists of indices: 'minima' with indices of values
- * that are local minima and 'maxlist' with indices of values that
- * are local maxima. Indices can be of any type.
- * Takes numbers as first parameter and an accuracy > 0 (yTolerance)
- * as second parameter. The accuracy has to be chosen depending
- * on the fluctuations in the data: smaller values mean greater
- * reliability in finding extrema but also greater chance of
- * confusing noise with a local minimum or maximum.
- *
- * @author Balint Morvai <balint@morvai.de>
- * @license http://en.wikipedia.org/wiki/MIT_License MIT License
- * @param values
- * @param yTolerance
- * @returns { minima: Array, maxima: Array }
- */
-export function localExtrema(values, yTolerance) {
-  // make yArray enumerated and define xArray = 1, 2, 3, ...
-  const valuesKeys = Object.keys(values);
-  const xArray = Array.from({ length: valuesKeys.length }).map((_, i) => i);
-  const yArray = getArray(values);
-  // call extremaXY version
-  const res = extremaXY(xArray, yArray, yTolerance);
-  // find interval centers
-  const centerKey = ([intervalStart, intervalEnd]) => (
-    valuesKeys[Math.floor((intervalStart + intervalEnd) * 0.5)]
-  );
-  res.minima = res.minima.map(centerKey);
-  res.maxima = res.maxima.map(centerKey);
-  return res;
-}
-
-/**
- * Alternative version that takes vectors [x] and [y] instead of
- * [values] as input and returns intervals that contain local minima
- * and local maxima. [x] elements can be of any type.
- *
  * For every extreme point, return one x interval.
  * The y values of the interval start and end
  * *can* be significantly different from the extreme point's y value.
  * This difference is minimized by the option `minimizeIntervals: true`.
  *
+ * yTolerance depends on the data fluctuations:
+ * smaller values mean greater reliability in finding extrema
+ * but also greater chance of confusing noise
+ * with a local minimum or maximum (false positives).
+ *
  * @author Balint Morvai <balint@morvai.de>
  * @license http://en.wikipedia.org/wiki/MIT_License MIT License
- * @param xArray
- * @param yArray
- * @param yTolerance
+ * @param data
+ * @param options
+ * @option yTolerance
+ * @option minimizeIntervals
+ * @option getX
+ * @option getY
+ * @option getLength
  * @returns { minima: Array, maxima: Array }
  */
-export function localExtremaXY(xArray, yArray, options) {
-  xArray = getArray(xArray);
-  yArray = getArray(yArray);
-  const defaultOptions = { yTolerance: 0.1, minimizeIntervals: true };
+export function localExtrema(data, options) {
+  const defaultOptions = {
+    yTolerance: 0.1,
+    minimizeIntervals: true,
+    getX: (data, idx) => idx,
+    getY: (data, idx) => data[idx],
+    getLength: (data) => data.length,
+  };
   options = Object.assign(defaultOptions, options);
   const { yTolerance, minimizeIntervals } = options;
   // TODO better way to find start of x intervals (detect plateaus) (avoid looping back)
   // TODO also return plateaus who are not min plateaus and not max plateaus
-  // TODO xArray is not used in this function, remove function parameter, return x indices
-  if (xArray.length < 3) {
+  const getLength = () => options.getLength(data);
+  const getX = (idx) => options.getX(data, idx);
+  const getY = (idx) => options.getY(data, idx);
+  const dataLength = getLength();
+  if (dataLength < 3) {
     // we need at least three points to analyze extreme points:
     // first derivation is zero and second derivation is nonzero
     return { minima: [], maxima: [] };
@@ -82,11 +55,11 @@ export function localExtremaXY(xArray, yArray, options) {
   let lastSlope; // -1: falling, +1: rising
   const maxima = [];
   const minima = [];
-  let lastMin = yArray[0];
-  let lastMax = yArray[0];
-  let lastY = yArray[0];
-  let x = xArray[1];
-  let y = yArray[1];
+  let lastMin = getY(0);
+  let lastMax = getY(0);
+  let lastY = getY(0);
+  let x = getX(1);
+  let y = getY(1);
   let i = 2; // next point
   let plateauLength = 0;
 
@@ -104,15 +77,15 @@ export function localExtremaXY(xArray, yArray, options) {
   else {
     // plateau at start of array
     // seek to end of plateau
-    for (; i < yArray.length; i++) {
-      y = yArray[i];
+    for (; i < dataLength; i++) {
+      y = getY(i);
       if (y < (lastMax - yTolerance)) {
         // maximum plateau + falling
         if (i > 1 && minimizeIntervals) {
-          maxima.push([xArray[0], xArray[i - 1]]);
+          maxima.push([getX(0), getX(i - 1)]);
         }
         else {
-          maxima.push([xArray[0], xArray[i]]);
+          maxima.push([getX(0), getX(i)]);
         }
         lastSlope = -1;
         lastMin = y;
@@ -121,10 +94,10 @@ export function localExtremaXY(xArray, yArray, options) {
       else if (y > (lastMin + yTolerance)) {
         // minimum plateau + rising
         if (i > 1 && minimizeIntervals) {
-          minima.push([xArray[0], xArray[i - 1]]);
+          minima.push([getX(0), getX(i - 1)]);
         }
         else {
-          minima.push([xArray[0], xArray[i]]);
+          minima.push([getX(0), getX(i)]);
         }
         lastSlope = 1;
         lastMax = y;
@@ -133,9 +106,9 @@ export function localExtremaXY(xArray, yArray, options) {
     }
   }
 
-  for (; i < yArray.length; i++) {
-    x = xArray[i];
-    y = yArray[i];
+  for (; i < dataLength; i++) {
+    x = getX(i);
+    y = getY(i);
     //console.log(`extremaXY: i ${i} + x ${x} + y ${y} + lastSlope ${lastSlope} + lastMin ${lastMin} + lastMax ${lastMax}`);
     if (lastSlope == 1) {
       // was rising
@@ -156,14 +129,14 @@ export function localExtremaXY(xArray, yArray, options) {
         plateauLength = 0;
         // find x interval of last maximum
         let iStart = i - 1;
-        while (yArray[iStart] >= lastMax - yTolerance) {
+        while (getY(iStart) >= lastMax - yTolerance) {
           iStart--;
         }
         if ((i - iStart) >= 2 && minimizeIntervals) {
-          maxima.push([xArray[iStart + 1], xArray[i - 1]]);
+          maxima.push([getX(iStart + 1), getX(i - 1)]);
         }
         else {
-          maxima.push([xArray[iStart], xArray[i]]);
+          maxima.push([getX(iStart), getX(i)]);
         }
         // (plateau or) falling
         lastSlope = -1; // change
@@ -189,14 +162,14 @@ export function localExtremaXY(xArray, yArray, options) {
         plateauLength = 0;
         // find x interval of last minimum
         let iStart = i - 1;
-        while (yArray[iStart] <= lastMin + yTolerance) {
+        while (getY(iStart) <= lastMin + yTolerance) {
           iStart--;
         }
         if ((i - iStart) >= 2 && minimizeIntervals) {
-          minima.push([xArray[iStart + 1], xArray[i - 1]]);
+          minima.push([getX(iStart + 1), getX(i - 1)]);
         }
         else {
-          minima.push([xArray[iStart], xArray[i]]);
+          minima.push([getX(iStart), getX(i)]);
         }
         // rising
         lastSlope = 1; // change
@@ -211,11 +184,11 @@ export function localExtremaXY(xArray, yArray, options) {
     // plateauLength is already minimal, no need to minimize
     if (y > lastMax - yTolerance) {
       // maximum plateau
-      maxima.push([xArray[i - plateauLength], xArray[i]]);
+      maxima.push([getX(i - plateauLength), getX(i)]);
     }
     else {
       // minimum plateau
-      minima.push([xArray[i - plateauLength], xArray[i]]);
+      minima.push([getX(i - plateauLength), getX(i)]);
     }
   }
   return { minima, maxima };
--- test/local-extrema-xy.test.js
+++ test/local-extrema-xy.test.js
@@ -8,84 +8,126 @@ function indexArray(data) {
 
 test('finds minimum in middle of array', () => {
   const array = [5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [], "minima": [[5, 5]]});
 });
 
 test('finds maximum in middle of array', () => {
   const array = [5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[5, 5]], "minima": []});
 });
 
 test('finds max + min + max', () => {
   const array = [5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[5, 5], [15, 15]], "minima": [[10, 10]]});
 });
 
 test('finds min + max + min', () => {
   const array = [10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[10, 10]], "minima": [[5, 5], [15, 15]]});
 });
 
 test('finds high plateau at start of array', () => {
   const array = [10, 10, 10, 10, 10, 9, 8, 7, 6, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[0, 4]], "minima": []});
 });
 
 test('finds high plateau in middle of array', () => {
   const array = [5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 9, 8, 7, 6, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[5, 9]], "minima": []});
 });
 
 test('finds high plateau at end of array', () => {
   const array = [5, 6, 7, 8, 9, 10, 10, 10, 10, 10];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[5, 9]], "minima": []});
 });
 
 test('finds high plateau at start and end of array', () => {
   const array = [10, 10, 10, 10, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[0, 4], [14, 18]], "minima": [[9, 9]]});
 });
 
 test('finds high plateau at start and middle and end of array', () => {
   const array = [10, 10, 10, 10, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[0, 4], [14, 18], [28, 32]], "minima": [[9, 9], [23, 23]]});
 });
 
 test('finds low plateau at start of array', () => {
   const array = [5, 5, 5, 5, 5, 6, 7, 8, 9, 10];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [], "minima": [[0, 4]]});
 });
 
 test('finds low plateau in middle of array', () => {
   const array = [10, 9, 8, 7, 6, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [], "minima": [[5, 9]]});
 });
 
 test('finds low plateau at end of array', () => {
   const array = [10, 9, 8, 7, 6, 5, 5, 5, 5, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [], "minima": [[5, 9]]});
 });
 
 test('finds low plateau at start and end of array', () => {
   const array = [5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
   expect(res).toStrictEqual({"maxima": [[9, 9]], "minima": [[0, 4], [14, 18]]});
 });
 
 test('finds low plateau at start and middle and end of array', () => {
   const array = [5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5];
-  const res = diffJS.localExtremaXY(indexArray(array), array);
+  const res = diffJS.localExtrema(array);
+  expect(res).toStrictEqual({"maxima": [[9, 9], [23, 23]], "minima": [[0, 4], [14, 18], [28, 32]]});
+});
+
+test('supports custom data format 1', () => {
+  const values = [5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5];
+  const data = {
+    keys: Array.from({ length: values.length }).map((_, i) => i),
+    values,
+  };
+  const res = diffJS.localExtrema(
+    data, {
+    getX: (data, idx) => data.keys[idx],
+    getY: (data, idx) => data.values[idx],
+    getLength: (data) => data.values.length,
+  });
+  expect(res).toStrictEqual({"maxima": [[9, 9], [23, 23]], "minima": [[0, 4], [14, 18], [28, 32]]});
+});
+
+test('supports custom data format 2', () => {
+  const values = [5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5];
+  const data = [
+    Array.from({ length: values.length }).map((_, i) => i),
+    values,
+  ];
+  const res = diffJS.localExtrema(
+    data, {
+    getX: (data, idx) => data[0][idx],
+    getY: (data, idx) => data[1][idx],
+    getLength: (data) => data[1].length,
+  });
+  expect(res).toStrictEqual({"maxima": [[9, 9], [23, 23]], "minima": [[0, 4], [14, 18], [28, 32]]});
+});
+
+test('supports custom data format 3', () => {
+  const values = [5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 5, 5, 5, 5];
+  const data = values.map((val, idx) => [idx, val]);
+  const res = diffJS.localExtrema(
+    data, {
+    getX: (data, idx) => data[idx][0],
+    getY: (data, idx) => data[idx][1],
+    getLength: (data) => data.length,
+  });
   expect(res).toStrictEqual({"maxima": [[9, 9], [23, 23]], "minima": [[0, 4], [14, 18], [28, 32]]});
 });
